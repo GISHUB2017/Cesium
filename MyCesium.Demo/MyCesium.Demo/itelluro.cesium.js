040178c05051f1c640b5e761e980b79cd3af6b3f
GISHUB2017 <943402175@qq.com>
2017-02-23 11:24:37 +0800

    添加工程文件

---
+++ MyCesium.Demo/MyCesium.Demo/itelluro.cesium.js
@@ -0,0 +1,18 @@
+﻿//cesium适配器
+function iTelluro() {
+
+
+}
+
+function NewWebTilesMap() {
+
+}
+
+function NewLocalTilesMap() {
+
+}
+
+function Map3DView(id){
+	var view = new Cesium.Viewer(id);
+	return view;
+}
